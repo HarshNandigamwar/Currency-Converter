2765e22de1ddc9a1b9c80701d2e2b3ef9d1cbdde
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-03-29 17:28:40 +0530

    Updating style

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // Updated base URL
-const BASE_URL =
-  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+const BASE_URL = "Your API URL";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
